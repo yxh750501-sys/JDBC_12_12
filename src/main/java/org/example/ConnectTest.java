1bcce2cef0d2048dfbc3a4241acd5ad8e319a3c3
yxh750501-sys <yxh750501@gmail.com>
2025-12-12 11:10:24 +0900

    JDBC 연결 테스트

---
+++ src/main/java/org/example/ConnectTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectTest {
+    public static void main(String[] args) {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+            conn = DriverManager.getConnection(url, "root", "");
+            System.out.println("연결 성공!");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("드라이버 로딩 실패" + e);
+        } catch (SQLException e) {
+            System.out.println("에러 : " + e);
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
